faffb3e0eb753dbc552f2064b149624b758e3e40
hrauxloh <h.rauxloh@gmail.com>
2025-08-28 14:25:44 +0200

    Data export checks upload

---
+++ Twitter_data_export_checks.R
@@ -0,0 +1,47 @@
+##
+library(dplyr)
+library(tidyr)
+
+# Path to your main folder
+main_dir <- "Twitter Data Collection Logs/Twitter Data Collection Logs"
+subfolders <- list.dirs(main_dir, full.names = TRUE, recursive = FALSE)
+
+
+## Sum of tweets per handle ----
+for (folder in subfolders) {
+
+  folder_name <- basename(folder)
+  files <- list.files(folder, pattern = "_collection_stats.csv$", full.names = TRUE)
+  df <- files %>%
+    lapply(read.csv) %>%
+    bind_rows()
+  df_summary <- df %>%
+    group_by(handle) %>%
+    summarise(num_tweets = sum(num_tweets, na.rm = TRUE), .groups = "drop")
+  assign(folder_name, df_summary, envir = .GlobalEnv)
+}
+
+
+## Appearence in multiple elections ----
+
+all_dfs <- mget(subfolders %>% basename(), envir = .GlobalEnv)
+handles_list <- lapply(names(all_dfs), function(name) {
+  data.frame(handle = all_dfs[[name]]$handle, folder = name, stringsAsFactors = FALSE)
+})
+all_handles <- bind_rows(handles_list)
+handle_counts <- all_handles %>%
+  distinct(handle, folder) %>%        
+  count(handle, name = "num_folders") %>%
+  filter(num_folders > 1) ## max appearance is 2 elections 
+
+
+## Summary num_tweets accross all elections ---- 
+handles_with_counts <- lapply(names(all_dfs), function(name) {
+  df <- all_dfs[[name]]
+  df$folder <- name
+  df
+})
+long_df <- bind_rows(handles_with_counts)
+wide_df <- long_df %>%
+  select(handle, folder, num_tweets) %>%
+  pivot_wider(names_from = folder, values_from = num_tweets, values_fill = 0)
